e9a68af8c785ab57ad4aeb2f17fc90148f8930ee
Raviteja Guttula <thisisgvrt@gmail.com>
2020-12-29 12:44:43 -0800

    Some prettification

--- components/ASRContainer.jsx
+++ components/ASRContainer.jsx
@@ -12,10 +12,23 @@ export default function ASRContainer({ timeStamp }) {
     const minimum_distance = min(distance_array);
     const minimum_distance_index = indexOf(distance_array, minimum_distance);
     const sentence = (groupedASR[groupedASRKeys[minimum_distance_index]]).map(elem => elem.word).join(" ");
+    let previous_sentence, next_sentence;
+    if(minimum_distance_index > 1){
+        previous_sentence = (groupedASR[groupedASRKeys[minimum_distance_index - 1]]).map(elem => elem.word).join(" ");
+    }
+    if(minimum_distance_index < distance_array.length - 1){
+        next_sentence = (groupedASR[groupedASRKeys[minimum_distance_index + 1]]).map(elem => elem.word).join(" ");
+    }
     return (
-        <div className="flex flex-col justify-center items-center bg-grey-lighter">
-            <div>
+        <div className="h-full w-full flex flex-col justify-around items-center text-center">
+            <div className="text-lg font-medium text-gray-600">
+                {previous_sentence}
+            </div>
+            <div className="text-xl font-semibold text-gray-800">
                 {sentence}
+            </div>
+            <div className="text-lg font-medium text-gray-600">
+                {next_sentence}
             </div>
         </div>
     )
