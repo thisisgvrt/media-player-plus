5d66de15b426017e2a1fdfe9d007fff2fa35a150
Raviteja Guttula <thisisgvrt@gmail.com>
2020-12-29 19:12:35 -0800

    constant length phrases with index at mid point to improve reading experience

--- components/ASRContainer.jsx
+++ components/ASRContainer.jsx
@@ -1,23 +1,50 @@
-import { metadata, asr_agg_threshold } from '../metadata.json';
+import { metadata, phrase_length } from '../metadata.json';
 import { groupBy, min, indexOf } from 'lodash';
 
 const { asr_word } = metadata;
 
-const groupedASR = groupBy(asr_word, elem => (elem.start - (elem.start % asr_agg_threshold)));
-const groupedASRKeys = Object.keys(groupedASR);
+const indexed_constant_length_phrases = {};
+
+let word_index=0;
+
+while(word_index < asr_word.length){
+    let phrase_accumulator = [];
+    while(phrase_accumulator.length < phrase_length)
+    {
+        phrase_accumulator.push(asr_word[word_index]);
+        word_index += 1;
+    }
+    const start_timestamp_for_first_word = phrase_accumulator[0].start;
+    const end_timestamp_for_last_word = phrase_accumulator[phrase_accumulator.length - 1].end;
+    /*
+        Two main changes from the last commit.
+        1. We now have a constant length words in phrases, this improves the reading experiences for two good reasons
+            1. The phrase length is more predictable.
+            2. Instead of longer sentences in horizontal direction as the pace increases, we will now 
+                have text scrolling faster in vertical direction. Since we are already doing that as part of 
+                keeping track of previous and next sentence, less stress on eyes :) 
+        2. The phrases are now indexed with timestamp at the middle of the sentence, this should fit well with 
+            the algorithm below. As we calculate the phrase's distance from its midpoint we would have a much smoother 
+            transition from one phrase to another. 
+    */
+    const mean_timestamp_for_phrase = (start_timestamp_for_first_word + end_timestamp_for_last_word) / 2;
+    indexed_constant_length_phrases[mean_timestamp_for_phrase] = phrase_accumulator;
+}
+
+const groupedASRKeys = Object.keys(indexed_constant_length_phrases);
 
 export default function ASRContainer({ timeStamp }) {
     
     const distance_array = groupedASRKeys.map(instant => Math.abs(timeStamp - instant));
     const minimum_distance = min(distance_array);
     const minimum_distance_index = indexOf(distance_array, minimum_distance);
-    const sentence = (groupedASR[groupedASRKeys[minimum_distance_index]]).map(elem => elem.word).join(" ");
+    const sentence = (indexed_constant_length_phrases[groupedASRKeys[minimum_distance_index]]).map(elem => elem.word).join(" ");
     let previous_sentence, next_sentence;
     if(minimum_distance_index > 1){
-        previous_sentence = (groupedASR[groupedASRKeys[minimum_distance_index - 1]]).map(elem => elem.word).join(" ");
+        previous_sentence = (indexed_constant_length_phrases[groupedASRKeys[minimum_distance_index - 1]]).map(elem => elem.word).join(" ");
     }
     if(minimum_distance_index < distance_array.length - 1){
-        next_sentence = (groupedASR[groupedASRKeys[minimum_distance_index + 1]]).map(elem => elem.word).join(" ");
+        next_sentence = (indexed_constant_length_phrases[groupedASRKeys[minimum_distance_index + 1]]).map(elem => elem.word).join(" ");
     }
     return (
         <div className="h-full w-full flex flex-col justify-around items-center text-center">
