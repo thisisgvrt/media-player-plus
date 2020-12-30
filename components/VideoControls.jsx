0b442b15ffe35af63040739a705dd877ebfd287f
Raviteja Guttula <thisisgvrt@gmail.com>
2020-12-29 20:12:40 -0800

    implemented seeker functionality for progress bar

--- components/VideoControls.jsx
+++ components/VideoControls.jsx
@@ -1,14 +1,26 @@
+import React, { useRef } from 'react';
+
 import { metadata } from '../metadata.json';
 
 const { duration } = metadata;
 
 export default function VideoControls({ timeStamp, setTimeStamp, isPlaying, setPlayingStatus }) {
-    const onClickFunc = () => setTimeStamp(timeStamp + 10);
+    const progress_bar_container = useRef(null);
+    const progress_bar_click_seeker = (event) => {
+        const {left, right} = progress_bar_container.current.getBoundingClientRect();
+        const mouse_click_location_x_co_ordinate = event.clientX;
+        const intended_seek_point = ((mouse_click_location_x_co_ordinate - left) * duration) / (right - left );
+        setTimeStamp(intended_seek_point);
+    }
     return (
         <div>
             <div className="relative pt-1">
-                <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-pink-200">
-                    <div style={{ width: `${(timeStamp * 100) / duration}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-pink-500"></div>
+                <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-pink-200"
+                    ref={progress_bar_container}
+                    onClick={progress_bar_click_seeker}>
+                    <div style={{ width: `${(timeStamp * 100) / duration}%` }} 
+                        className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-pink-500"
+                        ></div>
                 </div>
             </div>
             <div className="flex flex-row justify-between">
